211e19f782b1c48cc2ce6a32a4b8cf2c02130da6
laurencechan <laurencechan@qq.com>
2017-06-14 11:12:39 +0800

    statistics-resource_pool main.html add view function

--- openstack_dashboard/dashboards/statistics/resource_pool/views.py
+++ openstack_dashboard/dashboards/statistics/resource_pool/views.py
@@ -3,7 +3,10 @@ from horizon import tables
 from openstack_dashboard.dashboards.statistics.resource_pool \
     import tables as project_tables
 
+
 class MainIndexView(tables.DataTableView):
+    from openstack_dashboard.dashboards.main.main.views import MainIndexView
+    MainIndexView.get_data(request=None, context={}, hypervisor_stats_count={}, instance_stats_count={})
     table_class = project_tables.MainTable
     template_name = 'statistics/resource_pool/main.html'
     page_title = "main page"
