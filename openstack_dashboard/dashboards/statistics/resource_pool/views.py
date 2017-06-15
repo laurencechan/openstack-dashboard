23ccd8093e9de3dfc54b602e48283aa9f3aa656a
laurencechan <laurencechan@qq.com>
2017-06-15 09:33:07 +0800

    test

--- openstack_dashboard/dashboards/statistics/resource_pool/views.py
+++ openstack_dashboard/dashboards/statistics/resource_pool/views.py
@@ -5,8 +5,6 @@ from openstack_dashboard.dashboards.statistics.resource_pool \
 
 
 class MainIndexView(tables.DataTableView):
-    from openstack_dashboard.dashboards.main.main.views import MainIndexView
-    MainIndexView.get_data(request=None, context={}, hypervisor_stats_count={}, instance_stats_count={})
     table_class = project_tables.MainTable
     template_name = 'statistics/resource_pool/main.html'
     page_title = "main page"
